795937e56946ce67d19a310797cff363d9f99e7b
Akash Bhardwaj <akashbwj@gmail.com>
2020-12-16 23:22:39 +0530

    add to cart

---
+++ app/http/controllers/customers/cartController.js
@@ -0,0 +1,44 @@
+function cartController(){
+    return {
+        index(req,res){
+            res.render('customers/cart');
+        },
+        update(req,res){
+            /*
+            "cart's structure"
+            cart={
+                items:{
+                    pizzaId: {item: pizzaObject, qty=0},
+                },
+                totalQty:0,
+                totalPrice:0
+            }
+            */
+           //when adding first pizza, create the cart structure first
+            if(!req.session.cart){
+                req.session.cart={
+                    items:{},
+                    totalQty:0,
+                    totalPrice:0
+                }
+            }
+            let cart=req.session.cart
+
+            //check if item does not exist in the cart
+            if(!cart.items[req.body._id]){
+                cart.items[req.body._id]={
+                    item:req.body,
+                    qty:1
+                }
+                cart.totalQty=cart.totalQty+1;
+                cart.totalPrice=cart.totalPrice+req.body.price;
+            } else {
+                cart.items[req.body._id].qty=cart.items[req.body._id].qty+1;
+                cart.totalQty=cart.totalQty+1;
+                cart.totalPrice=cart.totalPrice+req.body.price;
+            }
+            return res.json({totalQty:req.session.cart.totalQty})
+        }
+    }
+}
+module.exports=cartController;
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
-console.log('hello from app.js');
+import axios from 'axios';
+import Noty from 'noty';
+
+let addToCart=document.querySelectorAll('.add-to-cart');
+let cartCounter=document.querySelector('#cartCounter');
+
+function updateCart(pizza){
+    axios.post('/update-cart',pizza).then(res=>{
+        cartCounter.innerText=res.data.totalQty;
+        new Noty({
+            type:'success',
+            timeout:1000, //hides the message again after 1 sec
+            text: 'Item added to cart',
+            progressBar:false,
+            // layout:'topLeft'
+        }).show();
+    }).catch(err=>{
+        new Noty({
+            type:'error',
+            timeout:1000, //hides the message again after 1 sec
+            text: 'Something went wrong',
+            progressBar:false,
+    }).show();
+    })
+}
+
+addToCart.forEach((btn)=>{
+    btn.addEventListener('click',(e)=>{
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza);
+    })
+})
